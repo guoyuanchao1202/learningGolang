09e724179efcd649138a5c698d48681d5c2f5f90
HoA-W <932552424@qq.com>
2021-05-10 18:46:28 +0800

    add

--- main.go
+++ main.go
@@ -1,11 +1,65 @@
 package main
 
-import (
-	"fmt"
-	 "learningGolang/add"
-)
+import "fmt"
+
+//func main() {
+//	еrr := errors.New("foo")
+//	var err error
+//	if еrr != nil {
+//		fmt.Printf("%T %v", err, err)
+//	}
+//}
+
+//func main() {
+//	m := map[string]string{
+//		"acbc": "x",
+//		"ad":   "x",
+//		"dad":  "xasd",
+//		"dfr":  "ded",
+//		"geg":  "dwf",
+//		"greg":"asda",
+//		"hyrh":"ger",
+//		"kuk":"Ger",
+//		"Fgry":"x",
+//	}
+//	for k, v := range m {
+//		fmt.Println(k, " --- ", v)
+//	}
+//	fmt.Println("---------------")
+//
+//	for k, v := range m {
+//		fmt.Println(k, " --- ", v)
+//	}
+//}
+
+type test struct {
+	a int64
+	b int64
+	c int64
+	d int64
+	e int64
+}
+
+type test1 struct {
+	t  test
+	t1 test
+	t2 test
+	t3 test
+}
 
 func main() {
-	res := add.Add(1, 2)
-	fmt.Println(res)
+	m := map[int]int{
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+	}
+	i := 0
+	for key, value := range m {
+		if i == 0 {
+			m[5] = 5
+		}
+		fmt.Println(key, value)
+		i++
+	}
 }
---
+++ slicelearning/ch1.go
@@ -0,0 +1,23 @@
+package slicelearning
+
+import "fmt"
+
+func demo1() {
+	var slice []int
+	fmt.Println(slice)
+	//
+	//slice1 := []int{1, 2, 3}
+	//fmt.Println(slice1)
+	//slice1 := *new([]int)
+	//slice1 = append(slice1, 10)
+	//fmt.Println(slice1)
+	//slice2 := []int{1, 2, 3, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	//fmt.Println(slice2)
+	//slice3 := make([]int, 5, 10)
+	//slice3[2] = 10
+	//fmt.Println(slice3)
+	//slice4 := slice3[1:]
+	//i := slice4[0]
+	//fmt.Println(i)
+	//fmt.Println(slice4)
+}
